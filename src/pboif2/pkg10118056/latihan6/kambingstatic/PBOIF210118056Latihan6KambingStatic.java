1a3b160ba3dc1a5827f66175672cda1925fdfe2c
Mirrazibrahim <55575435+Mirrazibrahim@users.noreply.github.com>
2019-09-26 13:03:00 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118056/latihan6/kambingstatic/PBOIF210118056Latihan6KambingStatic.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118056.latihan6.kambingstatic;
+
+/**
+ *
+ * @author 
+ * NAMA     : Mirrazibrahim
+ * KELAS    : IF-2
+ * NIM      : 10118056
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * kambingnya midun
+ */
+public class PBOIF210118056Latihan6KambingStatic {
+
+    public static int jumlahKambing;
+
+public class KambingStatic {
+//NAMA KAMBING sebagai konstata
+    public static final String NAMA_KAMBING = "MIDUN";
+}
+    public static void main(String[] args) {
+    PBOIF210118056Latihan6KambingStatic.jumlahKambing = 48000;
+        String NAMA_KAMBING = "MIDUN";
+        System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak " + PBOIF210118056Latihan6KambingStatic.jumlahKambing);
+    }
+}
